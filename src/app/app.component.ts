d73a49265137c6129286c97cc1c1e36922f534dd
Fábio Valentim <maitabom@gmail.com>
2025-07-12 14:50:57 -0300

    adicionar componente Rodape com template e estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./components/rodape/rodape.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent],
+  imports: [CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
